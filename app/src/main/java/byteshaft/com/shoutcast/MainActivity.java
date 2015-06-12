b109de30b8fcb17672c83c495f6fdaee52b8b322
Shahid Ghafoor <shgr8rb@gmail.com>
2015-06-13 00:09:59 +0500

    add service class

--- app/src/main/java/byteshaft/com/shoutcast/MainActivity.java
+++ app/src/main/java/byteshaft/com/shoutcast/MainActivity.java
@@ -1,38 +1,18 @@
 package byteshaft.com.shoutcast;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-
-import java.io.IOException;
-
-import wseemann.media.FFmpegMediaPlayer;
+import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends ActionBarActivity implements FFmpegMediaPlayer.OnPreparedListener {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        start();
-    }
-
-    private void start() {
-        FFmpegMediaPlayer mp = new FFmpegMediaPlayer();
-        mp.setOnPreparedListener(this);
-        String url = "rtmp://levelagency.com/shoutcast/caracol.stream";
-        try {
-            mp.setDataSource(url);
-            mp.prepareAsync();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onPrepared(FFmpegMediaPlayer fFmpegMediaPlayer) {
-        fFmpegMediaPlayer.start();
+        startService(new Intent(getApplicationContext(), StreamService.class));
     }
 }
